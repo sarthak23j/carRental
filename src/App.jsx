23f353607c6040a3c3f7755912693e080c098bfb
Sarthak <sarthak23j@gmail.com>
2024-11-10 00:02:08 +0530

    dead code removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./Layout"
 import Home from "./pages/Home"
-import Test from "./components/Test"
 import Vehicles from "./pages/Vehicles"
 import CarPage from "./pages/CarPage"
 import OurTeam from "./pages/OurTeam"
 import ContactUs from "./pages/ContactUs"
-import BookNow from "./pages/BookNow"
 import "./globalStyles.css"
 
 export default function App() {
@@ -19,7 +17,6 @@ export default function App() {
                     <Route path="/vehicles/:key/:brand/:model/:type/:price/:year/:fuel/:rating" element={<CarPage />}/>
                     <Route path="/our-team" element={<OurTeam />} />
                     <Route path="/contact-us" element={<ContactUs />} />
-                    <Route path="/book-now" element={<BookNow />} />
                 </Route>
             </Routes>
         </BrowserRouter>
